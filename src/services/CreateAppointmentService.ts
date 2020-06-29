b4eb2d6a8cec10d972500d14bc4f882ec78a01c2
brandaoplaster <brandaoplaster@gmail.com>
2020-06-29 09:25:26 -0300

    Installing lib reflect-metadata and rename model for models

--- src/services/CreateAppointmentService.ts
+++ src/services/CreateAppointmentService.ts
@@ -1,7 +1,7 @@
 import { startOfHour } from 'date-fns';
 import { getCustomRepository } from 'typeorm';
 
-import Appointment from '../model/Appointment';
+import Appointment from '../models/Appointment';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 
 interface Request {
