1665a1e32097a5b9a3a7df5f6d96a7a160a0d367
brandaoplaster <brandaoplaster@gmail.com>
2020-07-02 20:27:13 -0300

    Creating userService to create new users

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,11 +1,22 @@
 import { Router } from 'express';
 
+import CreateUserService from '../services/CreateUserService';
+
 const usersRouter = Router();
 
 usersRouter.post('/', async (request, response) => {
   try {
     const { name, email, password } = request.body;
-    return response.json();
+
+    const createUserService = new CreateUserService();
+
+    const user = await createUserService.execute({
+      name,
+      email,
+      password,
+    });
+
+    return response.json(user);
   } catch (e) {
     return response.status(400).json({ error: e.message });
   }
