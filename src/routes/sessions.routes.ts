68224b1458e67bf09a0b99681d7e9791ca2e3e95
brandaoplaster <brandaoplaster@gmail.com>
2020-07-02 22:29:58 -0300

    Creating authenticate user service for authenticate user

--- src/routes/sessions.routes.ts
+++ src/routes/sessions.routes.ts
@@ -1,10 +1,21 @@
 import { Router } from 'express';
 
+import AuthenticateUserService from '../services/AuthenticateUserService';
+
 const sessionsRouter = Router();
 
 sessionsRouter.post('/', async (request, response) => {
   try {
-    return response.json({ ok: true });
+    const { email, password } = request.body;
+
+    const userAuthenticate = new AuthenticateUserService();
+
+    const { user } = await userAuthenticate.execute({
+      email,
+      password,
+    });
+
+    return response.json({ user });
   } catch (e) {
     return response.status(400).json({ error: e.message });
   }
