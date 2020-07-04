f97fb34c2ad315f9fd9d3f791a12fd66c37786c4
brandaoplaster <brandaoplaster@gmail.com>
2020-07-04 12:15:33 -0300

    Creating middleware to authenticate the user

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '54e613d3f06704f29d02452f136b0fba',
+    expiresIn: '1d',
+  },
+};
--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,6 +1,7 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
+import authConfig from '../config/auth';
 
 import User from '../models/user';
 
@@ -31,9 +32,11 @@ class AuthenticateUserService {
     }
     delete user.password;
 
-    const token = sign({}, '54e613d3f06704f29d02452f136b0fba', {
+    const { secret, expiresIn } = authConfig.jwt;
+
+    const token = sign({}, secret, {
       subject: user.id,
-      expiresIn: '1d',
+      expiresIn,
     });
 
     return { user, token };
