21545baa809890f42db3aff8c0df291c24ab3465
brandaoplaster <brandaoplaster@gmail.com>
2020-07-23 17:06:04 -0300

    Adding test for appointments

--- src/modules/appoitments/services/CreateAppointmentService.spec.ts
+++ src/modules/appoitments/services/CreateAppointmentService.spec.ts
@@ -1,4 +1,5 @@
 import FakeAppointmentsRepository from '@modules/appoitments/repositories/fakes/FakeAppointmentsRepository';
+import AppError from '@shared/errors/AppError';
 import CreateAppointmentService from './CreateAppointmentService';
 
 describe('CreateAppointment', () => {
@@ -17,7 +18,24 @@ describe('CreateAppointment', () => {
     expect(appointment.user_id).toBe('9876');
   });
 
-  // it('should not be able to create two appointments on the same time', () => {
+  it('should not be able to create two appointments on the same time', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointmentService = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
 
-  // });
+    const appointmentDate = new Date(2020, 7, 23, 5);
+
+    await createAppointmentService.execute({
+      date: appointmentDate,
+      user_id: '9876',
+    });
+
+    expect(
+      createAppointmentService.execute({
+        date: appointmentDate,
+        user_id: '9876',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
 });
