487c4d456c5792c015948e37f738967a031a1b86
brandaoplaster <brandaoplaster@gmail.com>
2020-07-23 12:30:27 -0300

    Dependency infection applied for user service

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -2,8 +2,15 @@ import { container } from 'tsyringe';
 
 import IAppointmentsRespostory from '@modules/appoitments/repositories/IAppointmentsRepository';
 import AppointmentsRepository from '@modules/appoitments/infra/typeorm/repositories/AppointmentsRepository';
+import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
 
 container.registerSingleton<IAppointmentsRespostory>(
   'AppointmentsRepository',
   AppointmentsRepository,
+);
+
+container.registerSingleton<IUsersRepository>(
+  'UsersRepository',
+  UsersRepository,
 );
