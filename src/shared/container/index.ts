58f7d466beae8a686539bbec66bbc3b08711d8a3
brandaoplaster <brandaoplaster@gmail.com>
2020-07-27 12:31:43 -0300

    Register repository the userTokenRepository

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -5,6 +5,8 @@ import IAppointmentsRespostory from '@modules/appoitments/repositories/IAppointm
 import AppointmentsRepository from '@modules/appoitments/infra/typeorm/repositories/AppointmentsRepository';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
+import UserTokenRepository from '@modules/users/infra/typeorm/repositories/UserTokenRepository';
+import IUserTokensRespository from '@modules/users/repositories/IUserTokensRespository';
 
 container.registerSingleton<IAppointmentsRespostory>(
   'AppointmentsRepository',
@@ -14,4 +16,9 @@ container.registerSingleton<IAppointmentsRespostory>(
 container.registerSingleton<IUsersRepository>(
   'UsersRepository',
   UsersRepository,
+);
+
+container.registerSingleton<IUserTokensRespository>(
+  'UserTokenRepository',
+  UserTokenRepository,
 );
