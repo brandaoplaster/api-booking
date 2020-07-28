6c928ea523c83da6594d4a08212407673e7cf5ab
brandaoplaster <brandaoplaster@gmail.com>
2020-07-28 10:07:07 -0300

    Factoring test of create user service

--- src/modules/users/services/CreateUserService.spec.ts
+++ src/modules/users/services/CreateUserService.spec.ts
@@ -3,16 +3,19 @@ import FakeUsersRepository from '../repositories/fakes/FakeUsersRepositories';
 import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
 import CreateUserService from './CreateUserService';
 
-describe('CreateUser', () => {
-  it('should be able to create a new  user', async () => {
-    const fakeUsersRepository = new FakeUsersRepository();
-    const fakeHashProvider = new FakeHashProvider();
-    const createUserService = new CreateUserService(
-      fakeUsersRepository,
-      fakeHashProvider,
-    );
+let fakeUsersRepository: FakeUsersRepository;
+let fakeHashProvider: FakeHashProvider;
+let createUser: CreateUserService;
 
-    const user = await createUserService.execute({
+describe('CreateUser', () => {
+  beforeEach(() => {
+    fakeUsersRepository = new FakeUsersRepository();
+    fakeHashProvider = new FakeHashProvider();
+    createUser = new CreateUserService(fakeUsersRepository, fakeHashProvider);
+  });
+
+  it('should be able to create a new  user', async () => {
+    const user = await createUser.execute({
       name: 'Joe',
       email: 'joe@gmail.com',
       password: '123456',
@@ -21,22 +24,14 @@ describe('CreateUser', () => {
   });
 
   it('should not be able to create a new with same email from another', async () => {
-    const fakeUsersRepository = new FakeUsersRepository();
-    const fakeHashProvider = new FakeHashProvider();
-
-    const createUserService = new CreateUserService(
-      fakeUsersRepository,
-      fakeHashProvider,
-    );
-
-    await createUserService.execute({
+    await createUser.execute({
       name: 'Joe 2',
       email: 'joe2@gmail.com',
       password: '123456',
     });
 
     expect(
-      createUserService.execute({
+      createUser.execute({
         name: 'Joe',
         email: 'joe2@gmail.com',
         password: '123456',
