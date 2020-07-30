215ef2325b1a34d4a01f993462a2389aabc8465e
brandaoplaster <brandaoplaster@gmail.com>
2020-07-30 15:22:52 -0300

    Removing times shorter than the current time

--- src/modules/appointments/services/ListProviderDayAvailabilityService.spec.ts
+++ src/modules/appointments/services/ListProviderDayAvailabilityService.spec.ts
@@ -16,12 +16,16 @@ describe('ListDayAvailabilityService', () => {
   it('should be able to list the day availability from provider ', async () => {
     await fakeAppointmentsReposioty.create({
       user_id: '12we',
-      date: new Date(2020, 7, 30, 8, 0, 0),
+      date: new Date(2020, 7, 30, 14, 0, 0),
     });
 
     await fakeAppointmentsReposioty.create({
       user_id: '12we',
-      date: new Date(2020, 7, 30, 10, 0, 0),
+      date: new Date(2020, 7, 30, 17, 0, 0),
+    });
+
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+      return new Date(2020, 4, 20, 11).getTime();
     });
 
     const availability = listProviderDayAvailable.execute({
@@ -34,9 +38,11 @@ describe('ListDayAvailabilityService', () => {
     expect(availability).toEqual(
       expect.arrayContaining([
         { hour: 8, available: false },
-        { hour: 9, available: true },
+        { hour: 9, available: false },
         { hour: 10, available: false },
-        { hour: 11, available: true },
+        { hour: 14, available: false },
+        { hour: 15, available: true },
+        { hour: 17, available: false },
       ]),
     );
   });
